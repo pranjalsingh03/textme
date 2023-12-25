e8f1ce9e7b654c2b6a2c62a0682149583a7a1573
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-12-25 10:58:44 +0530

    added page of base64 converter

---
+++ src/components/Base64/Base64.js
@@ -0,0 +1,14 @@
+function Base64(){
+
+    return(
+        <>
+        <div className="container">
+        <h1>Enter the text:</h1>
+      <textarea className="form-control" id="myBox" rows="8"></textarea>
+      <button className="btn btn-primary" >Convert to Base64</button>
+        </div>
+        </>
+    );
+};
+
+export default Base64
