552a2080e978e76af12684d9ff9d025d7bd592de
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-01-12 17:55:46 +0530

    Add error component and router configuration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Error from './components/error';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Base64 from './components/Base64/Base64';
+// import about from './components/About/about';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+
+    element: <App />,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/base64",
+        element: <Base64/>,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
