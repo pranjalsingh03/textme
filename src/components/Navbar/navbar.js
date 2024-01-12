6cff0b7dcd298b23ac2532404cf3ed0435acf7cc
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-01-12 17:56:16 +0530

    Update Navbar component

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import Base64 from '../Base64/Base64.js'
+// import Base64 from '../Base64/Base64.js'
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand ps-3" href="/">TextMe</a>
-            <a href={Base64} className="navbar-brand">Base64</a>
+            <button className="navbar-brand">Base64</button>
+            <button className="navbar-brand">About</button>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 
                 <span className="navbar-toggler-icon"></span>
