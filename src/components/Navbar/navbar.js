48e9a99a4de1e855ebde7d93c934a6f81c25f472
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-13 18:05:26 +0530

    Update Navbar links to use react-router-dom

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom/dist';
 // import Base64 from '../Base64/Base64.js'
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand ps-3" href="/">TextMe</a>
-            <button className="navbar-brand">Base64</button>
-            <button className="navbar-brand">About</button>
+            <Link className="navbar-brand relative left-1" to="/">TextMe</Link>
+            <Link className="navbar-brand " to="/base64">Base64</Link>
+            <Link className="navbar-brand " to="/about">About</Link>
+
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 
                 <span className="navbar-toggler-icon"></span>
