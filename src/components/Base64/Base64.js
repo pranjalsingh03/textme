646c92571f255f1f0722c425e7696603933351c1
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-13 18:01:32 +0530

    added base64 converter

--- src/components/Base64/Base64.js
+++ src/components/Base64/Base64.js
@@ -1,15 +1,30 @@
-function Base64(){
+import React, { useState } from 'react';
 
-    return(
-        <>
-        {/* <div className="container">
-        <h1>Enter the text:</h1>
-      <textarea className="form-control" id="myBox" rows="8"></textarea>
-      <button className="btn btn-primary" >Convert to Base64</button>
-        </div> */}
-        <p>hello</p>
-        </>
-    );
-};
+function Base64() {
+  const [text, setText] = useState('');
+  const [base64, setBase64] = useState('');
+
+  const convertToBase64 = () => {
+    const base64String = btoa(text);
+    setBase64(base64String);
+  };
+
+  return (
+    <div>
+      <h1>Enter the text:</h1>
+      <textarea
+        className="form-control"
+        id="myBox"
+        rows="8"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button className="btn btn-primary my-1" onClick={convertToBase64}>
+        Convert to Base64
+      </button>
+      {base64 && <p>Base64: {base64}</p>}
+    </div>
+  );
+}
 
 export default Base64;
