24b8d2e69d91a2af18ad4b8975cd2e237530d570
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-13 19:40:24 +0530

    Update Navbar styling and links

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom/dist';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand relative left-1" to="/">TextMe</Link>
-            <Link className="navbar-brand " to="/base64">Base64</Link>
-            <Link className="navbar-brand " to="/about">About</Link>
+        <nav className="p-2 bg-light">
+            <Link className="text-2xl relative left-1" to="/">TextMe</Link>
+            <Link className="text-black-500 hover:text-blue-800 transition duration-200 ml-4 text-xl" to="/base64">Base64</Link>
+            <Link className="text-black-500 hover:text-blue-800 transition duration-200 ml-4 text-xl" to="/about">About</Link>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 
