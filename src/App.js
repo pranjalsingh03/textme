238e7e56b37bf616ab1aa0bf7e6d4aa47812c6b0
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-13 18:02:07 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import Aboutus from './components/About/about';
 import Hero from './components/Hero/hero';
+import Base64 from './components/Base64/Base64';
 import Navbar from './components/Navbar/navbar';
-// import {
-//   BrowserRouter as 
-//   Routes,
-//   Route,
-//   Router
-// } from 'react-router-dom';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    
-      <Navbar />
-      <div className="container my-8">
-        <Hero heading="Enter the text" />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container my-8">
+          <Routes>
+            <Route path="/about" element={<Aboutus/>} />
+            <Route path="/base64" element={<Base64/>} />
+            <Route path="/" element={<Hero heading="Enter the text" />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
