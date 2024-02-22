d3e2313bfeb997998ba90f9f9c45711bbcffe0ed
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-22 12:55:11 +0530

    Add Base64 decoding functionality and additional buttons

--- src/components/Base64/Base64.js
+++ src/components/Base64/Base64.js
@@ -8,6 +8,19 @@ function Base64() {
     const base64String = btoa(text);
     setBase64(base64String);
   };
+  const Base64ToText=()=>{
+    const textString = atob(base64);
+    setText(textString);
+  }
+  const handleCopytext=()=>{
+    navigator.clipboard.writeText(text);
+  }
+  const handleclearText=()=>{
+    setText('');
+  }
+  const handleCopyBase64=(e)=>{
+    setText(e.target.value);
+  }
 
   return (
     <div>
@@ -17,12 +30,16 @@ function Base64() {
         id="myBox"
         rows="8"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleCopyBase64}
       ></textarea>
       <button disabled={text.length===0} className="btn btn-primary my-1" onClick={convertToBase64}>
-        Convert to Base64
+        Encode to Base64
       </button>
+      <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={Base64ToText}>Decode from Base64</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopytext}>Copy text</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleclearText}>Clear</button>
       {base64 && <p className='py-2'>Base64: {base64}</p>}
+      {text && <p className='py-2'>Text: {text}</p>}
     </div>
   );
 }
