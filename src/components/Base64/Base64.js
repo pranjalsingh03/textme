2cfa19e62927ca1eaeee6f34236e67ab01451470
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-24 22:47:48 +0530

    Base64 component to add copy functionality

--- src/components/Base64/Base64.js
+++ src/components/Base64/Base64.js
@@ -19,8 +19,8 @@ function Base64() {
     setText('');
   }
   const handleCopyBase64=(e)=>{
-    setText(e.target.value);
-  }
+    navigator.clipboard.writeText(base64);
+    }
 
   return (
     <div>
@@ -30,14 +30,15 @@ function Base64() {
         id="myBox"
         rows="8"
         value={text}
-        onChange={handleCopyBase64}
-      ></textarea>
+        onChange={(e) => setText(e.target.value)}>
+      </textarea>
       <button disabled={text.length===0} className="btn btn-primary my-1" onClick={convertToBase64}>
         Encode to Base64
       </button>
       <button disabled={text.length===0} className="btn btn-primary my-1 mx-1" onClick={Base64ToText}>Decode from Base64</button>
       <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopytext}>Copy text</button>
       <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleclearText}>Clear</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopyBase64}>Copy Base64</button>
       {base64 && <p className='py-2'>Base64: {base64}</p>}
       {text && <p className='py-2'>Text: {text}</p>}
     </div>
